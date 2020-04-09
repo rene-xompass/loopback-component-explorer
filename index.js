b29888661c95a0aa84b3c069dbe2341f2ccf9366
Rene Mena <rene.mena.m@gmail.com>
2020-04-08 21:35:57 -0400

    change to xompass-loopback-swagger

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const url = require('url');
 const path = require('path');
 const urlJoin = require('./lib/url-join');
 const _defaults = require('lodash').defaults;
-const createSwaggerObject = require('loopback-swagger').generateSwaggerSpec;
+const createSwaggerObject = require('xompass-loopback-swagger').generateSwaggerSpec;
 const SWAGGER_UI_ROOT = require('swagger-ui/index').dist;
 const STATIC_ROOT = path.join(__dirname, 'public');
 
